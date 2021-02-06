a369fdb9d03e8cc744aa83a05f8f0e44f788dc09
آریا محب <ariya.mms@gmail.com>
2021-02-06 14:34:46 +0330

    Fb first post serivce

---
+++ src/main/java/ir/asta/training/warehouse/service/BookService.java
@@ -0,0 +1,22 @@
+package ir.asta.training.warehouse.service;
+
+import ir.asta.training.warehouse.dto.BookDto;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Slf4j
+@Path("book")
+public class BookService {
+
+    @POST
+    public Response postBook(BookDto bookDto) {
+        log.info(String.format("Request received to %s for %s = %s",
+                BookService.class.getSimpleName(),
+                BookDto.class.getSimpleName(),
+                bookDto));
+        return Response.noContent().build();
+    }
+}
