4d32f987c64e6f97445da7edab4e554736e26961
ariya-mms <ariya.mms@gmail.com>
2021-02-08 18:30:25 +0330

    Autowire bookDao

--- src/main/java/ir/asta/training/warehouse/service/BookService.java
+++ src/main/java/ir/asta/training/warehouse/service/BookService.java
@@ -1,15 +1,26 @@
 package ir.asta.training.warehouse.service;
 
+import ir.asta.training.warehouse.dao.BookDao;
 import ir.asta.training.warehouse.dto.BookDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
 @Slf4j
+@Component
 @Path("book")
 public class BookService {
+
+    private final BookDao bookDao;
+
+    @Autowired
+    public BookService(BookDao bookDao) {
+        this.bookDao = bookDao;
+    }
 
     @POST
     public Response postBook(BookDto bookDto) {
