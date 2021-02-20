5b8d691cda5aa82f871d6fdf9afd0e2c2788d337
ariya-mms <ariya.mms@gmail.com>
2021-02-21 01:42:21 +0330

    Delete HttpStatusCodes.java

--- src/main/java/ir/asta/training/warehouse/service/HttpStatusCodes.java
+++
@@ -1,6 +0,0 @@
-package ir.asta.training.warehouse.service;
-
-public class HttpStatusCodes {
-    public static final int UNPROCESSABLE_ENTITY_CODE = 422;
-    public static final String UNPROCESSABLE_ENTITY_MESSAGE = "Unprocessable Entity";
-}
