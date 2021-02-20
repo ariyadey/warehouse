642703b031112e3336d2c4e6a087fe1b4e380ce3
ariya-mms <ariya.mms@gmail.com>
2021-02-20 14:03:44 +0330

    Add BookEntity.java

--- src/main/java/ir/asta/training/warehouse/entity/BookEntity.java
+++ src/main/java/ir/asta/training/warehouse/entity/BookEntity.java
@@ -5,25 +5,29 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Entity
-@Builder
 @Data
-@NoArgsConstructor
+@Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class BookEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
+    @Column(nullable = false)
     private String title;
+
+    @Column(unique = true, nullable = false)
     private String isbn10;
+
+    @Column(unique = true, nullable = false)
     private String isbn13;
+
+    @Column(nullable = false)
     private BigDecimal price;
 }
