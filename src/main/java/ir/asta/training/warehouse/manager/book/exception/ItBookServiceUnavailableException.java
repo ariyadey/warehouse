046e6c8421b80ab7fc5b52d969b1ee4f8a69eeb0
ariya-mms <ariya.mms@gmail.com>
2021-02-21 02:00:23 +0330

    Delete ItBookServiceUnavailableException.java

--- src/main/java/ir/asta/training/warehouse/manager/book/exception/ItBookServiceUnavailableException.java
+++
@@ -1,8 +0,0 @@
-package ir.asta.training.warehouse.manager.book.exception;
-
-public class ItBookServiceUnavailableException extends RuntimeException {
-    private static final long serialVersionUID = 1330712223841053423L;
-
-    public ItBookServiceUnavailableException() {
-    }
-}
