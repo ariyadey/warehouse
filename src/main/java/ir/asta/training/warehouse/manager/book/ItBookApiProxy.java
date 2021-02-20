7eddb86922810c11f1211185c4e8f08bb1187901
ariya-mms <ariya.mms@gmail.com>
2021-02-20 21:46:45 +0330

    Refactor ItBookApiProxy.java

--- src/main/java/ir/asta/training/warehouse/manager/book/ItBookApiProxy.java
+++ src/main/java/ir/asta/training/warehouse/manager/book/ItBookApiProxy.java
@@ -2,11 +2,10 @@ package ir.asta.training.warehouse.manager.book;
 
 import ir.asta.training.warehouse.dto.ItBookApiDto;
 import ir.asta.training.warehouse.manager.book.exception.BookNotFoundException;
-import ir.asta.training.warehouse.manager.book.exception.ItBookServiceUnavailableException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
 
 import static java.lang.String.format;
@@ -22,28 +21,17 @@ public class ItBookApiProxy {
     }
 
     public ItBookApiDto load(String isbn13) {
-        if (client == null) {
-            client = ClientBuilder.newClient();
-        }
+        final ItBookApiDto dto = client
+                .target(format("https://api.itbook.store/1.0/books/%s", isbn13))
+                .request(MediaType.APPLICATION_JSON)
+                .get(ItBookApiDto.class);
+        validateDtoHasNoErrors(dto);
+        return dto;
+    }
 
-        ItBookApiDto dto;
-        try {
-            dto = client
-                    .target(format("https://api.itbook.store/1.0/books/%s", isbn13))
-                    .request(MediaType.APPLICATION_JSON)
-                    .get(ItBookApiDto.class);
-        } catch (RuntimeException exception) {
-            dto = null;
-        }
-
-        if (dto != null) {
-            if (dto.getError().equals("0")) {
-                return dto;
-            } else {
-                throw new BookNotFoundException();
-            }
-        } else {
-            throw new ItBookServiceUnavailableException();
+    private void validateDtoHasNoErrors(ItBookApiDto dto) {
+        if (!dto.getError().equals("0")) {
+            throw new BookNotFoundException();
         }
     }
 }
