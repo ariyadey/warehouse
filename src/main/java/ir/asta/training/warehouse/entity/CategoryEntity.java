e92ab04c33307e445ba5f1e2d4c86a7fb3e19dbf
ariya-mms <ariya.mms@gmail.com>
2021-02-25 17:26:02 +0330

    Add CategoryEntity.java

---
+++ src/main/java/ir/asta/training/warehouse/entity/CategoryEntity.java
@@ -0,0 +1,29 @@
+package ir.asta.training.warehouse.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity(name = "WH_CATEGORY")
+@Data
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class CategoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    @NotNull
+    private String code;
+
+    @Column(nullable = false)
+    @NotNull
+    private String subject;
+}
