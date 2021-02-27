4446023d101c5ccbbf74dacd3030bbf9af82cdd3
ariya-mms <ariya.mms@gmail.com>
2021-02-27 11:17:25 +0330

    Add CategoryIntegrationTest.java

---
+++ src/test/java/ir/asta/training/warehouse/manager/category/CategoryIntegrationTest.java
@@ -0,0 +1,41 @@
+package ir.asta.training.warehouse.manager.category;
+
+import ir.asta.training.warehouse.dto.CategorySaveRequestDto;
+import ir.asta.training.warehouse.entity.CategoryEntity;
+import ir.asta.training.warehouse.manager.category.exception.CategoryNotProcessableException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@AutoConfigureTestDatabase
+@Transactional
+class CategoryIntegrationTest {
+
+    @Autowired
+    CategoryManager manager;
+
+    @Test
+    void Should_Save_When_SubjectIsNotNullOrBlank() {
+        CategorySaveRequestDto givenDto = new CategorySaveRequestDto("A test subject");
+
+        CategoryEntity dbSavedEntity = manager.save(givenDto);
+        CategoryEntity dbLoadedEntity = manager.loadByCode(dbSavedEntity.getCode());
+
+        assertEquals(dbSavedEntity, dbLoadedEntity);
+    }
+
+    @Test
+    void ShouldNot_Save_When_SubjectIsNullOrBlank() {
+        CategorySaveRequestDto givenDto = new CategorySaveRequestDto("    ");
+
+        assertThrows(CategoryNotProcessableException.class, () -> manager.save(givenDto));
+    }
+
+
+}
