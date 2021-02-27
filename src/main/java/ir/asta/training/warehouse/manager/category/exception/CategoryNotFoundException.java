5493b9d0de30a0b5006a7986ab749eb911264dbd
ariya-mms <ariya.mms@gmail.com>
2021-02-27 11:29:52 +0330

    Add category-related exception classes

---
+++ src/main/java/ir/asta/training/warehouse/manager/category/exception/CategoryNotFoundException.java
@@ -0,0 +1,8 @@
+package ir.asta.training.warehouse.manager.category.exception;
+
+public class CategoryNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 3094491455972760599L;
+
+    public CategoryNotFoundException() {
+    }
+}
---
+++ src/main/java/ir/asta/training/warehouse/manager/category/exception/CategoryNotProcessableException.java
@@ -0,0 +1,8 @@
+package ir.asta.training.warehouse.manager.category.exception;
+
+public class CategoryNotProcessableException extends RuntimeException {
+    private static final long serialVersionUID = 4521726310692603683L;
+
+    public CategoryNotProcessableException() {
+    }
+}
