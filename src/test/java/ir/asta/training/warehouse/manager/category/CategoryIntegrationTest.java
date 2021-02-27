b10136605106286f4b37270e5e59474d6fd33fb2
ariya-mms <ariya.mms@gmail.com>
2021-02-27 12:40:51 +0330

    Update tests to increase coverage

--- src/test/java/ir/asta/training/warehouse/manager/category/CategoryIntegrationTest.java
+++ src/test/java/ir/asta/training/warehouse/manager/category/CategoryIntegrationTest.java
@@ -2,6 +2,7 @@ package ir.asta.training.warehouse.manager.category;
 
 import ir.asta.training.warehouse.dto.CategorySaveRequestDto;
 import ir.asta.training.warehouse.entity.CategoryEntity;
+import ir.asta.training.warehouse.manager.category.exception.CategoryNotFoundException;
 import ir.asta.training.warehouse.manager.category.exception.CategoryNotProcessableException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureTestDatabase
@@ -21,13 +21,16 @@ class CategoryIntegrationTest {
     CategoryManager manager;
 
     @Test
-    void Should_Save_When_SubjectHasText() {
+    void Should_SaveLoadDelete_When_SubjectHasText() {
         CategorySaveRequestDto givenDto = new CategorySaveRequestDto("A test subject");
 
         CategoryEntity dbSavedEntity = manager.save(givenDto);
         CategoryEntity dbLoadedEntity = manager.loadByCode(dbSavedEntity.getCode());
 
         assertEquals(dbSavedEntity, dbLoadedEntity);
+
+        assertDoesNotThrow(() -> manager.deleteByCode(dbSavedEntity.getCode()));
+        assertThrows(CategoryNotFoundException.class, () -> manager.loadByCode(dbSavedEntity.getCode()));
     }
 
     @Test
@@ -37,5 +40,14 @@ class CategoryIntegrationTest {
         assertThrows(CategoryNotProcessableException.class, () -> manager.save(givenDto));
     }
 
+    @Test
+    void ShouldNot_Load_When_EntityWithSpecifiedCodeDoesNotExist() {
+        assertThrows(CategoryNotFoundException.class, () -> manager.loadByCode("Entity with this code does not exist"));
+    }
 
+    @Test
+    void ShouldNot_Delete_When_EntityWithSpecifiedCodeDoesNotExist() {
+        assertThrows(CategoryNotFoundException.class,
+                () -> manager.deleteByCode("Entity with this code does not exist"));
+    }
 }
