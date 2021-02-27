5c854bc96b548771851a1e1e650ddccd2f23c88f
ariya-mms <ariya.mms@gmail.com>
2021-02-27 11:30:20 +0330

    Add CategoryManager.java

---
+++ src/main/java/ir/asta/training/warehouse/manager/category/CategoryManager.java
@@ -0,0 +1,41 @@
+package ir.asta.training.warehouse.manager.category;
+
+import ir.asta.training.warehouse.dao.CategoryDao;
+import ir.asta.training.warehouse.dto.CategorySaveRequestDto;
+import ir.asta.training.warehouse.entity.CategoryEntity;
+import ir.asta.training.warehouse.manager.category.exception.CategoryNotProcessableException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+import java.util.UUID;
+
+@Component
+public class CategoryManager {
+
+    private final CategoryDao dao;
+
+    @Autowired
+    public CategoryManager(CategoryDao dao) {
+        this.dao = dao;
+    }
+
+    @Transactional
+    public CategoryEntity save(CategorySaveRequestDto dto) {
+        validate(dto);
+        final CategoryEntity entity = new CategoryEntity(UUID.randomUUID().toString(), dto.getSubject());
+        return dao.save(entity);
+    }
+
+    @Transactional
+    public CategoryEntity loadByCode(String code) {
+        return dao.load(code);
+    }
+
+    private void validate(CategorySaveRequestDto dto) {
+        if (dto == null || !StringUtils.hasText(dto.getSubject())) {
+            throw new CategoryNotProcessableException();
+        }
+    }
+}
