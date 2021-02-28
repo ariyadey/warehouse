a19c4c692baab5c859f4e751ab8fd470b7028c04
ariya-mms <ariya.mms@gmail.com>
2021-02-28 11:40:01 +0330

    Delete unused CategoryNotProcessableException.java

--- src/main/java/ir/asta/training/warehouse/manager/category/exception/CategoryNotProcessableException.java
+++
@@ -1,8 +0,0 @@
-package ir.asta.training.warehouse.manager.category.exception;
-
-public class CategoryNotProcessableException extends RuntimeException {
-    private static final long serialVersionUID = 4521726310692603683L;
-
-    public CategoryNotProcessableException() {
-    }
-}
