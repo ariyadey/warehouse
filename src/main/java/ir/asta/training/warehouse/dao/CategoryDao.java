375c740316084ee650a54940e8026957fdf2c0b8
ariya-mms <ariya.mms@gmail.com>
2021-03-02 21:45:53 +0330

    Implement search operation

--- src/main/java/ir/asta/training/warehouse/dao/CategoryDao.java
+++ src/main/java/ir/asta/training/warehouse/dao/CategoryDao.java
@@ -1,5 +1,6 @@
 package ir.asta.training.warehouse.dao;
 
+import ir.asta.training.warehouse.dto.CategorySearchParamsDto;
 import ir.asta.training.warehouse.entity.CategoryEntity;
 import ir.asta.training.warehouse.manager.category.exception.CategoryNotFoundException;
 import lombok.extern.slf4j.Slf4j;
@@ -53,6 +54,57 @@ public class CategoryDao {
         log.debug("Removing the category from DB with code: {}", code);
         CategoryEntity entity = load(code);
         entityManager.remove(entity);
-        log.info("Category was removed from DB: {}", entity);
+        log.info("The category was removed from DB: {}", entity);
+    }
+
+    public List<CategoryEntity> search(CategorySearchParamsDto dto) {
+        log.debug("Searching in DB according to parameters: {}", dto);
+        String queryString = String.format("select c from CategoryEntity c\n" +
+                                           "%s" +
+                                           " %s %s %s\n" +
+                                           "order by %s c.id %s",
+                dto.getCode() == null && dto.getSubject() == null ? "" : "where",
+                dto.getCode() == null ? "" : "c.code = :code",
+                dto.getCode() == null || dto.getSubject() == null ? "" : "and",
+                dto.getSubject() == null ? "" : "lower(c.subject) like concat('%', lower(:subject), '%')",
+                dto.getOrderBy() == null ? "" : dto.getOrderBy() + ", ",
+                dto.getSortDirection());
+
+        TypedQuery<CategoryEntity> typedQuery = entityManager.createQuery(queryString, CategoryEntity.class);
+        if (dto.getCode() != null) {
+            typedQuery.setParameter("code", dto.getCode());
+        }
+        if (dto.getSubject() != null) {
+            typedQuery.setParameter("subject", dto.getSubject());
+        }
+        typedQuery.setMaxResults(dto.getPageSize());
+        typedQuery.setFirstResult(((dto.getPageNumber() - 1) * typedQuery.getMaxResults()));
+
+        List<CategoryEntity> searchResult = typedQuery.getResultList();
+        log.debug("Results are fetched according to search criteria: \n{}", searchResult);
+        return searchResult;
+    }
+
+
+    public long count(String code, String subject) {
+        log.debug("Counting categories with code: {} and subject: {}", code, subject);
+        String queryString = String.format("select count(c) from CategoryEntity c\n" +
+                                           "%s %s %s %s",
+                code == null && subject == null ? "" : "where",
+                code == null ? "" : "c.code = :code",
+                code == null || subject == null ? "" : "and",
+                subject == null ? "" : "lower(c.subject) like concat('%', lower(:subject), '%')");
+        Query query = entityManager.createQuery(queryString);
+
+        if (code != null) {
+            query.setParameter("code", code);
+        }
+        if (subject != null) {
+            query.setParameter("subject", subject);
+        }
+
+        final long result = (long) query.getSingleResult();
+        log.trace("The result of counting categories in DB is: {}", result);
+        return result;
     }
 }
--- src/main/java/ir/asta/training/warehouse/manager/category/CategoryManager.java
+++ src/main/java/ir/asta/training/warehouse/manager/category/CategoryManager.java
@@ -2,23 +2,30 @@ package ir.asta.training.warehouse.manager.category;
 
 import ir.asta.training.warehouse.dao.CategoryDao;
 import ir.asta.training.warehouse.dto.CategorySaveRequestDto;
+import ir.asta.training.warehouse.dto.CategorySearchParamsDto;
 import ir.asta.training.warehouse.entity.CategoryEntity;
-import lombok.extern.slf4j.Slf4j;
+import ir.asta.training.warehouse.manager.SortDirection;
+import ir.asta.training.warehouse.manager.category.exception.CategoryIllegalSearchException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
 
+import static ir.asta.training.warehouse.util.ReflectionUtil.containsField;
+
 @Component
-@Slf4j
 public class CategoryManager {
 
     private final CategoryDao dao;
+    private final Environment env;
 
     @Autowired
-    public CategoryManager(CategoryDao dao) {
+    public CategoryManager(CategoryDao dao, Environment env) {
         this.dao = dao;
+        this.env = env;
     }
 
     @Transactional
@@ -42,4 +49,37 @@ public class CategoryManager {
     public void deleteByCode(String code) {
         dao.remove(code);
     }
+
+    @Transactional(readOnly = true)
+    public List<CategoryEntity> search(CategorySearchParamsDto dto) {
+        validate(dto);
+        return dao.search(format(dto));
+    }
+
+    @Transactional(readOnly = true)
+    public long count(String code, String subject) {
+        return dao.count(code, subject);
+    }
+
+    private CategorySearchParamsDto format(CategorySearchParamsDto dto) {
+        return CategorySearchParamsDto.builder()
+                .subject(dto.getSubject())
+                .code(dto.getCode())
+                .pageSize(dto.getPageSize() == null || dto.getPageSize() < 0
+                        ? env.getRequiredProperty("warehouse.search.default-page-size", Integer.class)
+                        : dto.getPageSize())
+                .pageNumber(dto.getPageNumber() == null || dto.getPageNumber() < 0
+                        ? env.getRequiredProperty("warehouse.search.default-page-number", Integer.class)
+                        : dto.getPageNumber())
+                .orderBy(dto.getOrderBy())
+                .sortDirection(dto.getSortDirection() == null ? SortDirection.ASC : dto.getSortDirection())
+                .build();
+    }
+
+    private void validate(CategorySearchParamsDto dto) {
+        if (!containsField(CategoryEntity.class, dto.getOrderBy())) {
+            throw new CategoryIllegalSearchException(dto);
+        }
+    }
 }
+
